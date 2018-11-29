cd65d8b623d56a381f82330bc8185c6968f05627
Ian <ianhawe24@hotmail.co.uk>
2018-11-29 04:32:30 +0000

    array function added

---
+++ grid-maker-starter/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Add code here
+  const board = document.getElementById('board');
+
+});
+
+//2d Array done
+function arr2d(int1, int2) {
+  for (let i = 0; i < input1; i++) {
+
+    for (let j = 0; i < input2; j++) {
+
+      // i = 3 j = 10, i will be 3 lots of 10 opposed to 10 lots of 3
+      return i;
+
+    }
+  }
+}
